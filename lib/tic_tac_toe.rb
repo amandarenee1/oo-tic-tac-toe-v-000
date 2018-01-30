ded792e74bbfa3370067a5522a4fd668973f0804
Amanda Wingate <arw0219@gmail.com>
2018-01-30 00:44:38 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,7 +1,7 @@
 class TicTacToe
 
   WIN_COMBINATIONS = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[6,4,2]]
-  
+
   def initialize(board = nil)
       @board = board || Array.new(9, " ")
   end
@@ -13,49 +13,31 @@ class TicTacToe
     puts "-----------"
     puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
   end
-  
+
   def input_to_index(input)
   index = input.to_i-1
 end
 
-  def current_player
-    turn_count % 2 == 0 ? "X" : "O"
-  end
-
-  def turn_count
-    @board.count{|token| token == "X" || token == "O"}
-  end
-
-  
-
-  def current_player(board)
-    turns = turn_count(board)
-    if turns % 2 == 0
-      return "X"
-    else
-      return "O"
-  end
-
 def move(index, current_player = "X")
-    @board[index] = current_player
+  @board[index] = current_player
 end
 
 def position_taken?(index)
-  !(@board[index].nil? || @board[index] == " ")
+!(@board[index].nil? || @board[index] == " ")
 end
 
 def valid_move?(index)
-  index.between?(0,8) && !position_taken?(index)
+index.between?(0,8) && !position_taken?(index)
 end
 
 def turn_count
-  turn = 0
-  @board.each do |index|
-    if index == "X" || index == "O"
-      turn += 1
-    end
+turn = 0
+@board.each do |index|
+  if index == "X" || index == "O"
+    turn += 1
   end
-  return turn
+end
+return turn
 end
 
 end
