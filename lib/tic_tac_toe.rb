ab0e186050c795a50ab87f2b5322da9319c2d420
Amanda Wingate <arw0219@gmail.com>
2018-01-30 00:36:56 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -46,6 +46,16 @@ def valid_move?(index)
   index.between?(0,8) && !position_taken?(index)
 end
 
+def turn_count
+  turn = 0
+  @board.each do |index|
+    if index == "X" || index == "O"
+      turn += 1
+    end
+  end
+  return turn
+end
+
 end
 
 end
