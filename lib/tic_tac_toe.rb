2eaa57443db87f99270755469e15acdadcf88528
Amanda Wingate <arw0219@gmail.com>
2018-01-30 00:07:42 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,61 +1,51 @@
 class TicTacToe
-def initialize(board = nil)
-@board = board || Array.new(9, " ")
-end
+  def initialize(board = nil)
+      @board = board || Array.new(9, " ")
+  end
 
-def current_player
-turn_count % 2 == 0 ? "X" : "O"
-end
+  def current_player
+    turn_count % 2 == 0 ? "X" : "O"
+  end
 
-def turn_count
-@board.count { | token | token == "X" || token == "O"
-}
-end
+  def turn_count
+    @board.count{|token| token == "X" || token == "O"}
+  end
 
-def display_board
-puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
-puts "-----------"
-puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
-puts "-----------"
-puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
-end
+  def display_board
+    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+    puts "-----------"
+    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+    puts "-----------"
+    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+  end
 
-WIN_COMBINATIONS = [
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 5, 8],
-  [0, 4, 8],
-  [6, 4, 2]
-]
-puts "-----------"
+  WIN_COMBINATIONS = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[6,4,2]]
+    puts "-----------"
 
-def input_to_index(input)
-index = input.to_i - 1
-end
+  def input_to_index(input)
+    index = input.to_i-1
+  end
 
-def current_player(board)
-turns = turn_count(board)
-if turns % 2 == 0
-return "X"
-else
-  return "O"
-end
+  def current_player(board)
+    turns = turn_count(board)
+    if turns % 2 == 0
+      return "X"
+    else
+      return "O"
+  end
 
-def position_taken ? (board, position)
-if board[position] == " "
-false
-elsif board[position] == ""
-false
-elsif board[position] == nil
-false
-else board[position] == "X" || "O"
-true
-end
+  def position_taken?(board, position)
+    if board[position] == " "
+      false
+    elsif board[position] == ""
+      false
+    elsif board[position] == nil
+      false
+    else board[position] == "X" || "O"
+      true
+  end
 
-def move(board, position, char = "X")
-board[position] = char
-end
+  def move(board, position, char = "X")
+    board[position] = char
+  end
 end
